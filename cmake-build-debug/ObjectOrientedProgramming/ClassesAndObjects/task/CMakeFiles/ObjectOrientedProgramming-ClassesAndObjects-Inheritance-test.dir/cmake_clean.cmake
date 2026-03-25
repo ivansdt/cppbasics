c6de2f10a9a43212bf9215de758e33dd4b82d822
ivansdt <ivanpersonal2423@gmail.com>
2026-03-25 11:53:36 -0600

    Objects and Inheritance

---
+++ cmake-build-debug/ObjectOrientedProgramming/ClassesAndObjects/task/CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.dir/test/test.cpp.obj"
+  "CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.dir/test/test.cpp.obj.d"
+  "ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.exe"
+  "ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.exe.manifest"
+  "ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.pdb"
+  "libObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Inheritance-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
