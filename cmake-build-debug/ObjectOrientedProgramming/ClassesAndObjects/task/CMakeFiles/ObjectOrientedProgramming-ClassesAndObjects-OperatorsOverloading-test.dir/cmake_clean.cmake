19f0a3003a9f2c5521668c51d032f6387d10a985
Ivansdt2319 <ivanpersonal2423@gmail.com>
2026-03-25 09:51:53 -0600

    Operators Overloading

---
+++ cmake-build-debug/ObjectOrientedProgramming/ClassesAndObjects/task/CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.dir/test/test.cpp.obj"
+  "CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.dir/test/test.cpp.obj.d"
+  "ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.exe"
+  "ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.exe.manifest"
+  "ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.pdb"
+  "libObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-OperatorsOverloading-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/ObjectOrientedProgramming/ClassesAndObjects/task/CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.dir/test/test.cpp.obj"
+  "CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.dir/test/test.cpp.obj.d"
+  "ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.exe"
+  "ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.exe.manifest"
+  "ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.pdb"
+  "libObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ObjectOrientedProgramming-ClassesAndObjects-Polymorphism-test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
